4e5d78d13b4844f65d76c4e66eafb8a91fc8f682
Jack <zhuangjianguo@hotoa.com>
2019-03-12 22:46:26 +0800

    初始化

---
+++ src/components/global-header/index.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import { GlobalHeader,GlobalHeaderSearch,GlobalHeaderButton,GlobalHeaderDropdown,GlobalHeaderProfile } from '@salesforce/design-system-react';
+
+class SteedosGlobalHeader extends Component {
+  render() {
+    return (
+			<GlobalHeader
+				logoSrc="/assets/images/logo.svg"
+				onSkipToContent={() => {
+					console.log('>>> Skip to Content Clicked');
+				}}
+				onSkipToNav={() => {
+					console.log('>>> Skip to Nav Clicked');
+				}}
+			>
+				<GlobalHeaderSearch
+					placeholder="Search Salesforce"
+					onSelect={() => {
+						console.log('>>> onSelect');
+					}}
+					options={[{ label: 'Email' }, { label: 'Mobile' }]}
+				/>
+				<GlobalHeaderButton
+					className="slds-m-right_small"
+					iconVariant={null}
+					label="Feedback"
+					onClick={() => {
+						console.log('>>> onClick');
+					}}
+					variant="neutral"
+				/>
+				<GlobalHeaderDropdown
+					assistiveText={{ icon: 'Global Actions' }}
+					id="global-header-dropdown-example"
+					iconCategory="utility"
+					iconName="add"
+					onSelect={() => {
+						console.log('>>> onSelect');
+					}}
+					options={[{ label: 'New Note' }, { label: 'Log a Call' }]}
+				/>
+				<GlobalHeaderButton
+					assistiveText={{ icon: 'Help and Training' }}
+					iconCategory="utility"
+					iconName="question"
+					onClick={() => {
+						console.log('>>> onClick');
+					}}
+				/>
+				<GlobalHeaderButton
+					assistiveText={{ icon: 'Setup' }}
+					iconCategory="utility"
+					iconName="settings"
+					onClick={() => {
+						console.log('>>> onClick');
+					}}
+				/>
+				<GlobalHeaderProfile
+					avatar="/assets/images/avatar2.jpg"
+					id="global-header-profile-example"
+					onClick={() => {
+						console.log('>>> onClick');
+					}}
+					onSelect={() => {
+						console.log('>>> onSelect');
+					}}
+					options={[{ label: 'Profile Menu' }]}
+				/>
+			</GlobalHeader>
+		);
+  }
+}
+
+export default SteedosGlobalHeader;
---
+++ src/components/global-navigation-bar/index.js
@@ -0,0 +1,98 @@
+import React, { Component } from 'react';
+import { GlobalNavigationBar,GlobalNavigationBarRegion,GlobalNavigationBarLink,GlobalNavigationBarDropdown } from '@salesforce/design-system-react';
+import { AppLauncher,AppLauncherSection,AppLauncherTile } from '@salesforce/design-system-react';
+import { Button, Icon  } from '@salesforce/design-system-react';
+import "./style.css";
+
+class SteedosGlobalNavigationBar extends Component {
+    render() {
+		const dropdownCollection = [
+			{
+				label: 'Menu Item One',
+				value: '1',
+				iconCategory: 'utility',
+				iconName: 'table',
+				href: 'http://www.google.com',
+			},
+			{
+				label: 'Menu Item Two',
+				value: '2',
+				iconCategory: 'utility',
+				iconName: 'kanban',
+				href: 'http://www.google.com',
+			},
+			{
+				label: 'Menu Item Three',
+				value: '3',
+				iconCategory: 'utility',
+				iconName: 'side_list',
+				href: 'http://www.google.com',
+			},
+		];
+
+		return (
+				<GlobalNavigationBar>
+					<GlobalNavigationBarRegion region="primary">
+						<AppLauncher
+							triggerName="App Name"
+							modalHeaderButton={<Button label="App Exchange" />}
+						>
+							<AppLauncherSection title="Tile Section">
+								<AppLauncherTile
+									title="Marketing Cloud"
+									iconText="MC"
+									description="Send emails, track emails, read emails! Emails!"
+								/>
+								<AppLauncherTile
+									title="Call Center"
+									description="The key to call center and contact center is not to use too many words!"
+									descriptionHeading="Call Center"
+									iconText="CC"
+								/>
+							</AppLauncherSection>
+							<AppLauncherSection title="Small Tile Section">
+								<AppLauncherTile
+									title="Journey Builder"
+									iconText="JB"
+									size="small"
+								/>
+								<AppLauncherTile
+									title="Sales Cloud"
+									iconNode={
+										<Icon name="campaign" category="standard" size="large" />
+									}
+									size="small"
+								/>
+							</AppLauncherSection>
+						</AppLauncher>
+					</GlobalNavigationBarRegion>
+					<GlobalNavigationBarRegion region="secondary" navigation>
+						<GlobalNavigationBarLink
+							href="javascript:void(0);"
+							label="Home"
+							id="home-link"
+						/>
+						<GlobalNavigationBarDropdown
+							id="primaryDropdown"
+							assistiveText={{ icon: 'Context Menu Item 1' }}
+							label="Context Menu Item"
+							options={dropdownCollection}
+						/>
+						<GlobalNavigationBarLink
+							href="javascript:void(0);"
+							label="Context Menu Item 2"
+							active
+						/>
+					</GlobalNavigationBarRegion>
+					<GlobalNavigationBarRegion region="tertiary">
+						<GlobalNavigationBarLink
+							href="javascript:void(0);"
+							label="Actions"
+						/>
+					</GlobalNavigationBarRegion>
+				</GlobalNavigationBar>
+		);
+	}
+}
+
+export default SteedosGlobalNavigationBar;
