4b53e382cebb709849323299758f7d4eaaaeca68
Jack <zhuangjianguo@hotoa.com>
2019-03-13 22:59:17 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import { IconSettings } from "@salesforce/design-system-react";
-import SteedosApp from './components/app';
+import AppDemo from './components/app-demo';
 
 class App extends Component {
   render() {
     return (
 		<IconSettings iconPath="/assets/icons">
-			<SteedosApp/>
+			<AppDemo/>
 		</IconSettings>
 		);
   }
